b22b3396467ed7f9fa639c981fc3e374ab767cc7
Scott Knudstrup <scottknudstrup@gmail.com>
2025-04-07 10:28:47 -0700

    Use preexisting approach for configs registration

--- monty/run.py
+++ monty/run.py
@@ -7,28 +7,14 @@
 # license that can be found in the LICENSE file or at
 # https://opensource.org/licenses/MIT.
 
-"""Entrypoint for running an experiment."""
-
-import os
-
-from tbp.monty.frameworks.config_utils.cmd_parser import create_cmd_parser
 from tbp.monty.frameworks.run_env import setup_env
 
-from configs.experiments import CONFIGS
-from configs.names import NAMES
-
 setup_env()
+
+# Load all experiment configurations from local project
+from configs import CONFIGS  # noqa: E402
 
 from tbp.monty.frameworks.run import main  # noqa: E402
 
 if __name__ == "__main__":
-    cmd_args = None
-    cmd_parser = create_cmd_parser(experiments=NAMES)
-    cmd_args = cmd_parser.parse_args()
-    experiments = cmd_args.experiments
-
-    if cmd_args.quiet_habitat_logs:
-        os.environ["MAGNUM_LOG"] = "quiet"
-        os.environ["HABITAT_SIM_LOG"] = "quiet"
-
-    main(all_configs=CONFIGS, experiments=cmd_args.experiments)
+    main(all_configs=CONFIGS)
