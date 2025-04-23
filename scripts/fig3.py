92e3cf6cceadcb17ef2aac29aba673313c23b2d1
Scott Knudstrup <scottknudstrup@gmail.com>
2025-04-23 13:48:16 -0700

    Revert changes that belong in another branch/PR
    This reverts commit 9d9d60cba5cd692dcdf688c658ec0d08e6136410.
    
    Revert "thicker median line in fig 3 rotation error"
    
    This reverts commit 282f044fadc820c3c2f4f8c8aee246d67535ced9.
    
    Revert "Update docstrings and reorder function for new panel order"
    
    This reverts commit 04542c1eefb99fdcf1f1327b0be3d7490ee779c4.
    
    Revert "Update fig3.py"
    
    This reverts commit 18050e1b717eee897bdb26b0e973334562d9e8b7.
    
    Revert "Add MLH outlines"
    
    This reverts commit 197657616bc21879f4c0237b1b070228608b532f.

--- scripts/fig3.py
+++ scripts/fig3.py
@@ -8,25 +8,24 @@
 # https://opensource.org/licenses/MIT.
 """This module defines functions used to generate images for figure 3.
 
-Panel A: Known Objects
- - `plot_known_objects()`
-
-Panel B: Evidence Graphs and Patches
- - `plot_evidence_graphs_and_patches()`
-
-Panel C: Sensor Path
+Panel A: Sensor Path
  - `plot_sensor_path()`
 
-Panel D: Performance
+ Panel B: Performance
  - `plot_performance()`
  - `draw_icons()`
 
+ Panel C: Known Objects
+ - `plot_known_objects()`
+
+ Panel D: Evidence Graphs and Patches
+ - `plot_evidence_graphs_and_patches()`
+
 Running the above functions requires that the following experiments have been run:
- - `pretrain_dist_agent_1lm`: For plotting the known objects.
  - `fig3_evidence_run`: For plotting the sensor path and evidence graphs + patches.
  - `dist_agent_1lm`, `dist_agent_1lm_noise`, `dist_agent_1lm_randrot_all`, and
    `dist_agent_1lm_randrot_all_noise`: For plotting the performance metrics.
-
+ - `pretrain_dist_agent_1lm`: For plotting the known objects.
 """
 
 import matplotlib.pyplot as plt
@@ -54,248 +53,9 @@ init_matplotlib_style()
 OUT_DIR = DMC_ANALYSIS_DIR / "fig3"
 OUT_DIR.mkdir(parents=True, exist_ok=True)
 
-
 """
 --------------------------------------------------------------------------------
-Panel A: Known Objects
---------------------------------------------------------------------------------
-"""
-
-
-def plot_known_objects():
-    """Plot the "known objects" for panel A.
-
-    Requires the experiment `dist_agent_1lm` has been run.
-
-    Output is saved to `DMC_ANALYSIS_DIR/fig3/known_objects/`.
-    """
-    # Initialize output paths.
-    out_dir = OUT_DIR / "known_objects"
-    out_dir.mkdir(parents=True, exist_ok=True)
-
-    fig, axes = plt.subplots(1, 3, figsize=(5, 4), subplot_kw={"projection": "3d"})
-    mug = load_object_model("dist_agent_1lm", "mug")
-    bowl = load_object_model("dist_agent_1lm", "bowl")
-    golf_ball = load_object_model("dist_agent_1lm", "golf_ball")
-
-    axes[0].scatter(mug.x, mug.y, mug.z, color=mug.rgba, alpha=0.5, s=5, linewidth=0)
-    axes[1].scatter(
-        bowl.x, bowl.y, bowl.z, color=bowl.rgba, alpha=0.5, s=5, linewidth=0
-    )
-    axes[2].scatter(
-        golf_ball.x,
-        golf_ball.y,
-        golf_ball.z,
-        color=golf_ball.rgba,
-        alpha=0.5,
-        s=5,
-        linewidth=0,
-    )
-
-    for ax in axes:
-        axes3d_clean(ax, grid=False)
-        axes3d_set_aspect_equal(ax)
-        ax.view_init(115, -90, 0)
-
-    fig.savefig(out_dir / "known_objects.png")
-    fig.savefig(out_dir / "known_objects.svg")
-    plt.show()
-
-
-"""
---------------------------------------------------------------------------------
-Panel B: Evidence Graphs and Patches
---------------------------------------------------------------------------------
-"""
-
-
-def plot_evidence_graphs_and_patches():
-    """Plot the evidence graphs and patches for panel D.
-
-    Requires the experiment `fig3_evidence_run` has been run.
-
-    Output is saved to `DMC_ANALYSIS_DIR/fig3/evidence_graphs_and_patches/`.
-    """
-    # Initialize output paths.
-    main_out_dir = OUT_DIR / "evidence_graphs_and_patches"
-    main_out_dir.mkdir(parents=True, exist_ok=True)
-
-    # Load the stats.
-    experiment_dir = VISUALIZATION_RESULTS_DIR / "fig3_evidence_run"
-    detailed_stats = DetailedJSONStatsInterface(
-        experiment_dir / "detailed_run_stats.json"
-    )
-    stats = detailed_stats[0]
-
-    object_names = ["mug", "bowl", "golf_ball"]
-    steps = np.array([0, 10, 20, 39, 40])
-    steps = np.arange(41)
-    n_steps = len(steps)
-
-    n_rows = n_cols = 64
-    center_loc = n_rows // 2 * n_cols + n_cols // 2
-    centers = np.zeros((n_steps, 3))
-    for i in range(n_steps):
-        arr = np.array(stats["SM_0"]["raw_observations"][i]["semantic_3d"])
-        centers[i, 0] = arr[center_loc, 0]
-        centers[i, 1] = arr[center_loc, 1]
-        centers[i, 2] = arr[center_loc, 2]
-
-    # Extract evidence values for all objects.
-    all_evidences = stats["LM_0"]["evidences"]
-    all_possible_locations = stats["LM_0"]["possible_locations"]
-    all_possible_rotations = stats["LM_0"]["possible_rotations"]
-    objects = {name: load_object_model("dist_agent_1lm", name) for name in object_names}
-    for name, obj in objects.items():
-        obj.evidences, obj.locations = [], []
-        for i in range(n_steps):
-            obj.evidences.append(all_evidences[i][name])
-            obj.locations.append(all_possible_locations[i][name])
-        obj.evidences = np.array(obj.evidences)
-        obj.locations = np.array(obj.locations)
-        obj.rotation = np.array(all_possible_rotations[0][name])
-
-    # Plot evidence graphs for each object and step individually.
-    out_dir = main_out_dir / "evidence_graphs"
-    out_dir.mkdir(parents=True, exist_ok=True)
-    png_dir = out_dir / "png"
-    png_dir.mkdir(parents=True, exist_ok=True)
-    svg_dir = out_dir / "svg"
-    svg_dir.mkdir(parents=True, exist_ok=True)
-
-    for i, step in enumerate(steps):
-        fig, axes = plt.subplots(
-            1, len(objects) + 1, figsize=(12, 6), subplot_kw=dict(projection="3d")
-        )
-
-        # Plot mug with current observation location.
-        ax = axes[0]
-        mug = objects["mug"]
-        ax.scatter(
-            mug.x,
-            mug.y,
-            mug.z,
-            color="gray",
-            alpha=0.1,
-            s=1,
-            linewidths=0,
-        )
-        center = centers[step]
-        ax.scatter(center[0], center[1], center[2], color="red", s=50)
-        ax.view_init(100, -100, -10)
-        axes3d_clean(ax, grid=False)
-        axes3d_set_aspect_equal(ax)
-        ax.set_title(f"step {step}")
-        ax.set_xlim(-0.119, 0.119)
-        ax.set_ylim(1.5 - 0.119, 1.5 + 0.119)
-        ax.set_zlim(-0.119, 0.119)
-
-        # Make colormap for this step.
-        all_evidences = [obj.evidences[step].flatten() for obj in objects.values()]
-        all_evidences = np.concatenate(all_evidences)
-        evidences_min = np.percentile(all_evidences, 2.5)
-        evidences_max = np.percentile(all_evidences, 99.99)
-        scalar_map = plt.cm.ScalarMappable(
-            cmap="inferno", norm=plt.Normalize(vmin=evidences_min, vmax=evidences_max)
-        )
-
-        for j, obj in enumerate(objects.values()):
-            ax = axes[j + 1]
-            locations = obj.locations[step]
-            n_points = locations.shape[0] // 2
-            locations = locations[:n_points]
-            evidences = obj.evidences[step]
-            ev1 = evidences[:n_points]
-            ev2 = evidences[n_points:]
-            stacked = np.hstack([ev1[:, np.newaxis], ev2[:, np.newaxis]])
-            evidences = stacked.max(axis=1)
-
-            colors = evidences
-            sizes = np.log(evidences - evidences.min() + 1) * 10
-            alphas = np.array(evidences)
-            alphas = (alphas - alphas.min()) / (alphas.max() - alphas.min())
-            x, y, z = locations[:, 0], locations[:, 1], locations[:, 2]
-            ax.scatter(
-                x,
-                y,
-                z,
-                c=colors,
-                cmap="inferno",
-                alpha=alphas,
-                vmin=evidences_min,
-                vmax=evidences_max,
-                s=sizes,
-                linewidths=0,
-            )
-
-            # Plot highest evidence location separately.
-            ind_ev_max = evidences.argmax()
-            ev = evidences[ind_ev_max]
-            x, y, z = x[ind_ev_max], y[ind_ev_max], z[ind_ev_max]
-            sizes = sizes[ind_ev_max]
-            ax.scatter(
-                x,
-                y,
-                z,
-                c=ev,
-                cmap="inferno",
-                alpha=1,
-                vmin=evidences_min,
-                vmax=evidences_max,
-                s=sizes,
-                linewidths=0.5,
-                edgecolor="black",
-            )
-
-            ax.view_init(100, -100, -10)
-            axes3d_clean(ax, grid=False)
-            axes3d_set_aspect_equal(ax)
-            ax.set_title(f"step {step}")
-            ax.set_xlim(-0.119, 0.119)
-            ax.set_ylim(1.5 - 0.119, 1.5 + 0.119)
-            ax.set_zlim(-0.119, 0.119)
-        fig.subplots_adjust(left=0.05, right=0.95, bottom=0.1, top=0.9, wspace=0.2)
-        fig.savefig(png_dir / f"evidence_graphs_{step}.png")
-        fig.savefig(svg_dir / f"evidence_graphs_{step}.svg")
-        plt.close(fig)
-
-    # Plot the colorbar.
-    fig, ax = plt.subplots(1, 1, figsize=(1, 2))
-    cbar = plt.colorbar(scalar_map, ax=ax, orientation="vertical", label="Evidence")
-    ax.remove()  # Remove the empty axes, we just want the colorbar
-    cbar.set_ticks([])
-    cbar.set_label("")
-    fig.tight_layout()
-    fig.savefig(out_dir / "colorbar.png")
-    fig.savefig(out_dir / "colorbar.svg")
-    plt.close(fig)
-
-    # Extract RGBA patches for sensor module 0.
-    rgba_patches = []
-    for ind in range(n_steps):
-        rgba_patches.append(np.array(stats["SM_0"]["raw_observations"][ind]["rgba"]))
-    rgba_patches = np.array(rgba_patches)
-
-    # Save the RGBA patches.
-    out_dir = main_out_dir / "patches"
-    out_dir.mkdir(parents=True, exist_ok=True)
-    png_dir = out_dir / "png"
-    png_dir.mkdir(parents=True, exist_ok=True)
-    svg_dir = out_dir / "svg"
-    svg_dir.mkdir(parents=True, exist_ok=True)
-    for step in steps:
-        patch = rgba_patches[step]
-        fig, ax = plt.subplots(1, 1, figsize=(2, 2))
-        ax.imshow(patch)
-        ax.axis("off")
-        fig.tight_layout(pad=0.0)
-        fig.savefig(png_dir / f"patch_step_{step}.png")
-        fig.savefig(svg_dir / f"patch_step_{step}.svg")
-        plt.close(fig)
-
-"""
---------------------------------------------------------------------------------
-Panel C: Sensor Path
+Panel A: Sensor Path
 --------------------------------------------------------------------------------
 """
 
@@ -344,7 +104,7 @@ def plot_sensor_path():
 
 """
 --------------------------------------------------------------------------------
-Panel D: Performance
+Panel B: Performance
 --------------------------------------------------------------------------------
 """
 
@@ -386,7 +146,7 @@ def plot_performance() -> None:
     xticks = np.arange(4) * 1.3
     bar_positions = xticks - bar_width / 2 - gap / 2
     violin_positions = xticks + violin_width / 2 + gap / 2
-    median_style = dict(color="lightgray", lw=2, ls="-")
+    median_style = dict(color="lightgray", lw=1, ls="-")
 
     # Plot accuracy bars
     ax1.bar(
@@ -514,9 +274,250 @@ def draw_icons():
     plt.show()
 
 
+"""
+--------------------------------------------------------------------------------
+Panel C: Known Objects
+--------------------------------------------------------------------------------
+"""
+
+
+def plot_known_objects():
+    """Plot the "known objects" for panel A.
+
+    Requires the experiment `dist_agent_1lm` has been run.
+
+    Output is saved to `DMC_ANALYSIS_DIR/fig3/known_objects/`.
+    """
+    # Initialize output paths.
+    out_dir = OUT_DIR / "known_objects"
+    out_dir.mkdir(parents=True, exist_ok=True)
+
+    fig, axes = plt.subplots(1, 3, figsize=(5, 4), subplot_kw={"projection": "3d"})
+    mug = load_object_model("dist_agent_1lm", "mug")
+    bowl = load_object_model("dist_agent_1lm", "bowl")
+    golf_ball = load_object_model("dist_agent_1lm", "golf_ball")
+
+    axes[0].scatter(mug.x, mug.y, mug.z, color=mug.rgba, alpha=0.5, s=5, linewidth=0)
+    axes[1].scatter(
+        bowl.x, bowl.y, bowl.z, color=bowl.rgba, alpha=0.5, s=5, linewidth=0
+    )
+    axes[2].scatter(
+        golf_ball.x,
+        golf_ball.y,
+        golf_ball.z,
+        color=golf_ball.rgba,
+        alpha=0.5,
+        s=5,
+        linewidth=0,
+    )
+
+    for ax in axes:
+        axes3d_clean(ax, grid=False)
+        axes3d_set_aspect_equal(ax)
+        ax.view_init(115, -90, 0)
+
+    fig.savefig(out_dir / "known_objects.png")
+    fig.savefig(out_dir / "known_objects.svg")
+    plt.show()
+
+
+"""
+--------------------------------------------------------------------------------
+Panel D: Evidence Graphs and Patches
+--------------------------------------------------------------------------------
+"""
+
+
+def plot_evidence_graphs_and_patches():
+    """Plot the evidence graphs and patches for panel D.
+
+    Requires the experiment `fig3_evidence_run` has been run.
+
+    Output is saved to `DMC_ANALYSIS_DIR/fig3/evidence_graphs_and_patches/`.
+    """
+    # Initialize output paths.
+    main_out_dir = OUT_DIR / "evidence_graphs_and_patches"
+    main_out_dir.mkdir(parents=True, exist_ok=True)
+
+    # Load the stats.
+    experiment_dir = VISUALIZATION_RESULTS_DIR / "fig3_evidence_run"
+    detailed_stats = DetailedJSONStatsInterface(
+        experiment_dir / "detailed_run_stats.json"
+    )
+    stats = detailed_stats[0]
+
+    object_names = ["mug", "bowl", "golf_ball"]
+    steps = np.array([0, 10, 20, 39, 40])
+    steps = np.arange(41)
+    n_steps = len(steps)
+
+    n_rows = n_cols = 64
+    center_loc = n_rows // 2 * n_cols + n_cols // 2
+    centers = np.zeros((n_steps, 3))
+    for i in range(n_steps):
+        arr = np.array(stats["SM_0"]["raw_observations"][i]["semantic_3d"])
+        centers[i, 0] = arr[center_loc, 0]
+        centers[i, 1] = arr[center_loc, 1]
+        centers[i, 2] = arr[center_loc, 2]
+
+    # Extract evidence values for all objects.
+    all_evidences = stats["LM_0"]["evidences"]
+    all_possible_locations = stats["LM_0"]["possible_locations"]
+    all_possible_rotations = stats["LM_0"]["possible_rotations"]
+    objects = {
+        name: load_object_model("dist_agent_1lm_10distinctobj", name)
+        for name in object_names
+    }
+    for name, obj in objects.items():
+        obj.evidences, obj.locations = [], []
+        for i in range(n_steps):
+            obj.evidences.append(all_evidences[i][name])
+            obj.locations.append(all_possible_locations[i][name])
+        obj.evidences = np.array(obj.evidences)
+        obj.locations = np.array(obj.locations)
+        obj.rotation = np.array(all_possible_rotations[0][name])
+
+    # Plot evidence graphs for each object and step individually.
+    out_dir = main_out_dir / "evidence_graphs"
+    out_dir.mkdir(parents=True, exist_ok=True)
+    png_dir = out_dir / "png"
+    png_dir.mkdir(parents=True, exist_ok=True)
+    svg_dir = out_dir / "svg"
+    svg_dir.mkdir(parents=True, exist_ok=True)
+
+    for i, step in enumerate(steps):
+        fig, axes = plt.subplots(
+            1, len(objects) + 1, figsize=(12, 6), subplot_kw=dict(projection="3d")
+        )
+
+        # Plot mug with current observation location.
+        ax = axes[0]
+        mug = objects["mug"]
+        ax.scatter(
+            mug.x,
+            mug.y,
+            mug.z,
+            color="gray",
+            alpha=0.1,
+            s=1,
+            linewidths=0,
+        )
+        center = centers[step]
+        ax.scatter(center[0], center[1], center[2], color="red", s=50)
+        ax.view_init(100, -100, -10)
+        axes3d_clean(ax, grid=False)
+        axes3d_set_aspect_equal(ax)
+        ax.set_title(f"step {step}")
+        ax.set_xlim(-0.119, 0.119)
+        ax.set_ylim(1.5 - 0.119, 1.5 + 0.119)
+        ax.set_zlim(-0.119, 0.119)
+
+        # Make colormap for this step.
+        all_evidences = [obj.evidences[step].flatten() for obj in objects.values()]
+        all_evidences = np.concatenate(all_evidences)
+        evidences_min = np.percentile(all_evidences, 2.5)
+        evidences_max = np.percentile(all_evidences, 99.99)
+        scalar_map = plt.cm.ScalarMappable(
+            cmap="inferno", norm=plt.Normalize(vmin=evidences_min, vmax=evidences_max)
+        )
+
+        for j, obj in enumerate(objects.values()):
+            ax = axes[j + 1]
+            locations = obj.locations[step]
+            n_points = locations.shape[0] // 2
+            locations = locations[:n_points]
+            evidences = obj.evidences[step]
+            ev1 = evidences[:n_points]
+            ev2 = evidences[n_points:]
+            stacked = np.hstack([ev1[:, np.newaxis], ev2[:, np.newaxis]])
+            evidences = stacked.max(axis=1)
+
+            colors = evidences
+            sizes = np.log(evidences - evidences.min() + 1) * 10
+            alphas = np.array(evidences)
+            alphas = (alphas - alphas.min()) / (alphas.max() - alphas.min())
+            x, y, z = locations[:, 0], locations[:, 1], locations[:, 2]
+            ax.scatter(
+                x,
+                y,
+                z,
+                c=colors,
+                cmap="inferno",
+                alpha=alphas,
+                vmin=evidences_min,
+                vmax=evidences_max,
+                s=sizes,
+                linewidths=0,
+            )
+
+            # Plot highest evidence location separately.
+            ind_ev_max = evidences.argmax()
+            ev = evidences[ind_ev_max]
+            x, y, z = x[ind_ev_max], y[ind_ev_max], z[ind_ev_max]
+            sizes = sizes[ind_ev_max]
+            ax.scatter(
+                x,
+                y,
+                z,
+                c=ev,
+                cmap="inferno",
+                alpha=1,
+                vmin=evidences_min,
+                vmax=evidences_max,
+                s=sizes,
+                linewidths=0,
+            )
+
+            ax.view_init(100, -100, -10)
+            axes3d_clean(ax, grid=False)
+            axes3d_set_aspect_equal(ax)
+            ax.set_title(f"step {step}")
+            ax.set_xlim(-0.119, 0.119)
+            ax.set_ylim(1.5 - 0.119, 1.5 + 0.119)
+            ax.set_zlim(-0.119, 0.119)
+        fig.subplots_adjust(left=0.05, right=0.95, bottom=0.1, top=0.9, wspace=0.2)
+        fig.savefig(png_dir / f"evidence_graphs_{step}.png")
+        fig.savefig(svg_dir / f"evidence_graphs_{step}.svg")
+        plt.close(fig)
+
+    # Plot the colorbar.
+    fig, ax = plt.subplots(1, 1, figsize=(1, 2))
+    cbar = plt.colorbar(scalar_map, ax=ax, orientation="vertical", label="Evidence")
+    ax.remove()  # Remove the empty axes, we just want the colorbar
+    cbar.set_ticks([])
+    cbar.set_label("")
+    fig.tight_layout()
+    fig.savefig(out_dir / "colorbar.png")
+    fig.savefig(out_dir / "colorbar.svg")
+    plt.close(fig)
+
+    # Extract RGBA patches for sensor module 0.
+    rgba_patches = []
+    for ind in range(n_steps):
+        rgba_patches.append(np.array(stats["SM_0"]["raw_observations"][ind]["rgba"]))
+    rgba_patches = np.array(rgba_patches)
+
+    # Save the RGBA patches.
+    out_dir = main_out_dir / "patches"
+    out_dir.mkdir(parents=True, exist_ok=True)
+    png_dir = out_dir / "png"
+    png_dir.mkdir(parents=True, exist_ok=True)
+    svg_dir = out_dir / "svg"
+    svg_dir.mkdir(parents=True, exist_ok=True)
+    for step in steps:
+        patch = rgba_patches[step]
+        fig, ax = plt.subplots(1, 1, figsize=(2, 2))
+        ax.imshow(patch)
+        ax.axis("off")
+        fig.tight_layout(pad=0.0)
+        fig.savefig(png_dir / f"patch_step_{step}.png")
+        fig.savefig(svg_dir / f"patch_step_{step}.svg")
+        plt.close(fig)
+
+
 if __name__ == "__main__":
-    plot_known_objects()
-    plot_evidence_graphs_and_patches()
     plot_sensor_path()
     plot_performance()
     draw_icons()
+    plot_known_objects()
+    plot_evidence_graphs_and_patches()
