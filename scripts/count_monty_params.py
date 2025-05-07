b0ac6135bedb454a28d75ef09305180aac77cf6e
Niels Leadholm <33281346+nielsleadholm@users.noreply.github.com>
2025-05-07 14:15:49 +0100

    feat: Monty model parameter-counting script (#18)

---
+++ scripts/count_monty_params.py
@@ -0,0 +1,165 @@
+# Copyright 2025 Thousand Brains Project
+#
+# Copyright may exist in Contributors' modifications
+# and/or contributions to the work.
+#
+# Use of this source code is governed by the MIT
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+
+"""Count the number of parameters in the `dist_agent_1lm`  Monty model.
+
+This script counts the number of parameters in a Monty model, specifically the
+`dist_agent_1lm` pretrained model. This produces the result (4 million parameters)
+referenced in the Continual Learning section of the paper.
+
+The Monty model consists of a series of learned graphs for different objects. These
+are stored in a nested state dictionary, e.g.:
+
+    graph = state_dict["lm_dict"][lm_id]["graph_memory"]["mug"]["patch"]
+
+This script will count all the numerical values stored in these graphs, as a measure
+of the total number of parameters in the model.
+
+For clarity, we begin by breaking down the structure of the state dictionary.
+
+A Monty model's state_dict contains the following keys:
+- 'lm_dict'
+- 'sm_dict'
+- 'motor_system_dict'
+- 'lm_to_lm_matrix'
+- 'lm_to_lm_vote_matrix'
+- 'sm_to_lm_matrix'
+
+All of the information we are concerned with is in lm_dict. sm_dict and
+motor_system_dict contain information for logging and visualization purposes. The
+connectivity information in lm_to_lm_matrix, lm_to_lm_vote_matrix, and sm_to_lm_matrix
+is a negligible fraction of data, particularly in single-LM models.
+
+Within lm_dict, lm_id is the index of the Learning Module(s), of which only 0 is
+relevant for single-LM models. lm_dict does not contain any other keys.
+
+state_dict["lm_dict"][lm_id] contains:
+- 'graph_memory'
+- 'target_to_graph_id'
+- 'graph_id_to_target'
+The latter two are mappings between the object name and the index of the graph, which
+are needed to assess the performance of the model. 
+
+state_dict["lm_dict"][lm_id]["graph_memory"] contains keys corresponding
+to all of the learned objects (e.g. 77 in the case of YCB).
+
+Each graph key (e.g. state_dict["lm_dict"][lm_id]["graph_memory"]["hammer"])
+is associated with a key corresponding to the sensory channel that was used to learn the
+object. This is typically "patch", but could be indexed (e.g. "patch_0", "patch_1") in
+multi-LM (hierarchical or voting) models.
+
+A graph can therefore be accessed with e.g.
+state_dict["lm_dict"][lm_id]["graph_memory"]["hammer"]["patch"].
+
+The accessed graph is a torch_geometric Data object, which has the following structure:
+
+Data(
+    x=[1188, 28],  # 28 features per node; not all of these are used in inference,
+    # but for simplicity we count all of them.
+    pos=[1188, 3],  # Position of each node in an object's reference frame
+    norm=[1188, 3],  # The norm of each node; this is redundant with the norm in x,
+    # and is in general never accessed directly during inference.
+
+    # feature_mapping stores the mapping between the array indices in x, and the
+    # features they represent. For example, "pose_vectors" has values [1, 10],
+    # indicating that indices 1:10 in x store the pose vectors of the node. These are
+    # stored as utility variables as Monty frequently switches between using the full x
+    # array, vs indexing individual values.
+    feature_mapping={
+        node_ids=[2],
+        pose_vectors=[2],
+        pose_fully_defined=[2],
+        on_object=[2],
+        object_coverage=[2],
+        rgba=[2],
+        hsv=[2],
+        principal_curvatures=[2],
+        principal_curvatures_log=[2],
+        gaussian_curvature=[2],
+        mean_curvature=[2],
+        gaussian_curvature_sc=[2],
+        mean_curvature_sc=[2]
+    },
+
+    # Edges are not used in Monty models at the moment and so can be ignored.
+    edge_index=[2, 13068],
+    edge_attr=[13068, 3]
+)
+"""
+
+import torch
+from typing import Union, List
+
+# Get the value of the DMC_ROOT_DIR environment variable, or use "~/tbp/results/dmc" as
+# the default if not set.
+from data_utils import DMC_ROOT_DIR
+
+# We use the single-LM, distant-agent model.
+DIST_AGENT_1LM_MODEL_PATH = (DMC_ROOT_DIR /
+                        "pretrained_models/dist_agent_1lm/pretrained/model.pt")
+SENSORY_CHANNEL = "patch"
+LM_ID = 0
+
+def count_monty_model_params(
+        graph_memory: dict,
+        sensory_channels_input: Union[str, List[str]]
+) -> int:
+    """Count the number of parameters in a Monty model.
+
+    Args:
+        graph_memory (dict): The graph memory dictionary of the model.
+        sensory_channels_input (Union[str, List[str]]): The sensory channel name or a
+            list of sensory channel names for the learned graphs for which to count
+            parameters.
+
+    Returns:
+        The total number of parameters in the model for the specified sensory
+        channel(s).
+    """
+
+    total_params = 0
+
+    if isinstance(sensory_channels_input, str):
+        channels_to_process = [sensory_channels_input]
+    else:
+        channels_to_process = sensory_channels_input
+
+    # Iterate through the learned objects
+    for object_data_for_all_channels in graph_memory.values():
+        # object_data_for_all_channels is a dict like {"patch": data} or
+        # {"patch_0": data, "patch_1": data_1, ...} for a single object
+        for channel_name in channels_to_process:
+            if channel_name in object_data_for_all_channels:
+                torch_geometric_data = object_data_for_all_channels[channel_name]
+
+                total_params += torch_geometric_data.x.numel()
+                total_params += torch_geometric_data.pos.numel()
+
+                # Each feature mapping key has two values (the upper and lower bounds of
+                # the indices in the x array that store the feature).
+                total_params += len(torch_geometric_data.feature_mapping.keys()) * 2
+
+        # Due to redundancy/not being used, norm and edge_index/edge_attr are not
+        # counted.
+
+    return total_params
+
+
+if __name__ == "__main__":
+    graph_memory = torch.load(
+        DIST_AGENT_1LM_MODEL_PATH
+    )["lm_dict"][LM_ID]["graph_memory"]
+
+    total_params = count_monty_model_params(graph_memory, SENSORY_CHANNEL)
+
+    print("Monty Model Parameter Count")
+    print(f" - Model: {DIST_AGENT_1LM_MODEL_PATH}")
+    print(f" - Sensory Channel(s): '{SENSORY_CHANNEL}'")
+    print(f" - LM ID: {LM_ID}")
+    print(f" - Total number of parameters: {total_params} (~{total_params / 1e6:.0f}M)")
