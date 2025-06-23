f1639e98e2cbb24aaa4da35701ca26f9c2e15de8
Niels Leadholm <33281346+nielsleadholm@users.noreply.github.com>
2025-06-23 16:53:38 +0100

    feat: Minor adjustments to figure styles (#44)

--- scripts/fig6.py
+++ scripts/fig6.py
@@ -211,11 +211,12 @@ def plot_performance():
         performance["percent.correct_mlh"],
         bottom=performance["percent.correct"],
         width=0.8,
-        color=TBP_COLORS["yellow"],
+        color=TBP_COLORS["blue"],
+        alpha=0.3,
     )
     ax.set_xticks(xticks)
     ax.set_xticklabels(xticklabels, rotation=45, ha="center")
-    ax.set_ylabel("% Correct")
+    ax.set_ylabel("Accuracy (%)")
     ax.set_ylim(0, 100)
     ax.legend(["Converged", "Timed-Out"], loc="lower right", framealpha=1)
 
@@ -233,7 +234,7 @@ def plot_performance():
 
     ax.set_xticks(xticks)
     ax.set_xticklabels(xticklabels, rotation=45, ha="center")
-    ax.set_ylabel("Steps")
+    ax.set_ylabel("Number of Steps")
     ax.set_ylim(0, 500)
     fig.tight_layout()
     fig.savefig(out_dir / "performance.png", bbox_inches="tight")
